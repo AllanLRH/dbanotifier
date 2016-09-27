33c30e303e7bc3d2d99b475e2e7860326ebbfbe2
Allan L. R. Hansen <allan.lrh@gmail.com>
2016-09-27 15:43:16 +0200

    Added logging

---
+++ TODO.md
@@ -0,0 +1,8 @@
+# TODO
+
+* Rename repo
+* Add logging
+* Better error handling
+* Parse config-file at commandline, support multiple files
+* test-suite?
+* Scheaduling in config-file
--- boliaSofa.py
+++ boliaSofa.py
@@ -7,6 +7,7 @@ import datetime
 import html
 import os
 import re
+import logging
 # Rest of the imports not in stdlib
 import requests
 import yaml
@@ -16,6 +17,8 @@ from tinydb import TinyDB, Query
 
 with open(os.path.join(os.path.dirname(__file__), "config.yaml")) as fid:
     cfg = yaml.load(fid)
+
+logging.basicConfig(filename=cfg["logfilePath"], level=logging.INFO)
 
 User = Query()
 db = TinyDB(cfg["dbPath"])
@@ -28,6 +31,7 @@ def convertDatestringToDate(inStr):
     Convert date string to datetime.date-object.
     If not possible, return inStr.
     """
+    logging.info("Converting {} to datetime".format(inStr))
     currentYear = datetime.datetime.today().year
     monthMap = {"jan": 1,
                 "feb": 2,
@@ -41,26 +45,38 @@ def convertDatestringToDate(inStr):
                 "okt": 10,
                 "nov": 11,
                 "dec": 12}
-    m = re.match(r'(\d+)\. (\w+)', inStr)
-    if m:
-        d, m = m.groups()
-        return datetime.date(currentYear, monthMap[m], int(d))
-    elif inStr == "I dag":
-        return datetime.date.today()
-    elif inStr == "I går":
-        return datetime.date.today() - datetime.timedelta(days=1)
-    else:
-        return inStr
+    try:
+        toReturn = None
+        m = re.match(r'(\d+)\. (\w+)', inStr)
+        if m:
+            d, m = m.groups()
+            toReturn = datetime.date(currentYear, monthMap[m], int(d))
+        elif inStr == "I dag":
+            toReturn = datetime.date.today()
+        elif inStr == "I går":
+            toReturn = datetime.date.today() - datetime.timedelta(days=1)
+        else:
+            toReturn = inStr
+        if toReturn == inStr:
+            logging.critical("Could not convert string '{}' to datetime-object".format(inStr))
+            return toReturn
+    except Exception as e:
+        logging.critical("An error occured when attempting to convert the string '{}' to a datetime-object".format(inStr))
+        raise e
 
 
 def getSoup(url):
     """
     Get webpage specified by url, return result as a BeautifulSoup-object.
     """
-    r = requests.get(url)
-    if r.status_code == 200:
-        return BeautifulSoup(r.text, 'lxml')
-    print('Error retrieving DBA search\n%s' % url, file=sys.stderr)
+    logging.info("Getting soup for url: " + url)
+    try:
+        r = requests.get(url)
+        if r.status_code == 200:
+            return BeautifulSoup(r.text, 'lxml')
+    except requests.ConnectionError as e:
+        logging.critical("Could not get html for url {}, status code {}. Captured error message: {}".format(url, r.status_code, e))
+        raise e
 
 
 def extractInfo(s0):
@@ -69,12 +85,17 @@ def extractInfo(s0):
     Returns results in a dict.
     """
     s1 = s0.findAll(class_="dbaListing")
+    logging.info("{} listings found in search".format(len(s1)))
     itemList = list()
     for el in s1:
         url = el.find('a', class_="thumbnailContainerInner")['href']
+        logging.debug("Found listing url: {}".format(url))
         itemId = url.split('/')[-2].replace('id-', '')
+        logging.debug("Found listing itemId: {}".format(itemId))
         price = el.find('td', title="Pris").text.strip()
+        logging.debug("Found listing price: {}".format(price))
         d0 = el.find('td', title="Dato").text.strip()
+        logging.debug("Found listing date-string: {}".format(d0))
         date = convertDatestringToDate(d0)
         # Extract shortened title from <script>
         t0 = el.find("script")
@@ -84,7 +105,10 @@ def extractInfo(s0):
         t4 = t3.replace('"', '')
         t5 = t4.rstrip(",")
         title = html.unescape(re.sub(r'&amp;(?=\#\d+)', r'&', t5))
-        itemList.append({'itemId': itemId, 'url': url, 'date': date, 'price': price, 'title': title})
+        logging.debug("Found listing title: {}".format(title))
+        listingDict = {'itemId': itemId, 'url': url, 'date': date, 'price': price, 'title': title}
+        itemList.append(listingDict)
+        logging.info("Listing dict: {}".format(listingDict))
     return itemList
 
 
@@ -93,18 +117,26 @@ def updateDatabase(searchResult):
     Update database with new search results, and send a push
     message if there's any new items in the search result.
     """
+    logging.debug("The search result is: {}".format(searchResult))
     pb = Pushbullet(cfg["pushBulletId"])
+    logging.info("Created pushbullet instance with id {}".format(cfg["pushBulletId"]))
+    logging.info("Checking database against {} search results".format(len(searchResult)))
     toUpdate = [el for el in searchResult if not db.search(User.itemId == el['itemId'])]
+    logging.info("There was {} new results which will be appended to the database".format(len(toUpdate)))
+    logging.debug("The following will be appended to the database: {}".format(toUpdate))
     messageList = list()
     for el in toUpdate:
         db.insert(el)
         messageList.append("{title}\n({date}), {price}\n{url}".format(**el))
     if messageList:
+        logging.info("The following message will be sent: {}".format("\n\n".join(messageList)))
         try:
             pb.push_note("Nyt fra DBA", "\n\n".join(messageList))
         except:
             for el in toUpdate:
                 webbrowser.open_new_tab(el["url"])
+    else:
+        logging.info("No message will be sent")
 
 
 if __name__ == '__main__':
